db93732004fe15fa81fa22bcfb76bf44fdc3a182
taras <dyshkotaras@gmail.com>
2023-01-07 21:35:25 +0200

    Lesson #49 Third screen Logic

--- app/src/main/java/com/dyshkotaras/cafe/MainActivity.java
+++ app/src/main/java/com/dyshkotaras/cafe/MainActivity.java
@@ -4,14 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonSignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,OrderDetailActivity.class);
-        startActivity(intent);
+        initViews();
+        buttonSignIn.setOnClickListener((View View) -> launchMakeOrderActivity(getText(editTextName), getText(editTextPassword)));
     }
+
+    private void initViews() {
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+    }
+
+    public void launchMakeOrderActivity(String userName, String password) {
+        if (userName.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                    MainActivity.this,
+                    R.string.error_fields_empty,
+                    Toast.LENGTH_LONG).show();
+        } else {
+            startActivity(MakeOrderActivity.newIntent(MainActivity.this, userName));
+        }
+    }
+
+    public String getText(EditText editText) {
+        return editText.getText().toString().trim();
+    }
+
 }
