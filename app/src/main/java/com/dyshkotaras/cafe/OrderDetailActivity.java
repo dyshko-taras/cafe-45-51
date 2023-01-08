90e0d3f86cf62fabcfdae3de1ef29bd55ee7ed3b
taras <dyshkotaras@gmail.com>
2023-01-08 18:19:00 +0200

    Lesson #51 Third screen logic

--- app/src/main/java/com/dyshkotaras/cafe/OrderDetailActivity.java
+++ app/src/main/java/com/dyshkotaras/cafe/OrderDetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,10 +16,34 @@ public class OrderDetailActivity extends AppCompatActivity {
     private static final String EXTRA_ADDITIVES = "additives";
     private static final String EXTRA_DRINK_TYPE = "drinkType";
 
+    private TextView textViewUserName;
+    private TextView textViewDrink;
+    private TextView textViewDrinkType;
+    private TextView textViewAdditives;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        initView();
+        getInfo();
+
+    }
+
+    private void getInfo() {
+
+        String userName = getIntent().getStringExtra(EXTRA_USER_NAME);
+        textViewUserName.setText(userName);
+
+        String drink = getIntent().getStringExtra(EXTRA_DRINK);
+        textViewDrink.setText(drink);
+
+        ArrayList<String> additives = getIntent().getStringArrayListExtra(EXTRA_ADDITIVES);
+        textViewAdditives.setText(additives.toString());
+
+        String drinkType = getIntent().getStringExtra(EXTRA_DRINK_TYPE);
+        textViewDrinkType.setText(drinkType);
     }
 
     public static Intent newIntent(
@@ -33,5 +58,12 @@ public class OrderDetailActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_ADDITIVES,additives);
         intent.putExtra(EXTRA_DRINK_TYPE,drinkType);
         return intent;
+    }
+
+    public void initView() {
+        textViewUserName = findViewById(R.id.textViewUserName);
+        textViewDrink = findViewById(R.id.textViewDrink);
+        textViewDrinkType = findViewById(R.id.textViewDrinkType);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
     }
 }
